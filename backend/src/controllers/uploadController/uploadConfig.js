aafc9b12f42ba75bb17dc9bf6d216fe4ed163c4e
MohammadG2002 <157503349+MohammadG2002@users.noreply.github.com>
2026-01-21 16:00:18 +0200

    - Added more upload formats - Increased upload size limit from 10MB to 1GB

--- backend/src/controllers/uploadController/uploadConfig.js
+++ backend/src/controllers/uploadController/uploadConfig.js
@@ -52,7 +52,7 @@ if (useCloudinary) {
       const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
       cb(
         null,
-        file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+        file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname),
       );
     },
   });
@@ -95,7 +95,7 @@ const fileFilter = (req, file, cb) => {
 export const upload = multer({
   storage,
   limits: {
-    fileSize: 10 * 1024 * 1024, // 10MB limit
+    fileSize: 1024 * 1024 * 1024, // 1GB limit
   },
   fileFilter,
 });
